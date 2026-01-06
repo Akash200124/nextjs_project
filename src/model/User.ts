b7855ef48b46719336056ae4c5ae99f1a6441ecb
unknown <akashkumar200124@gmail.com>
2026-01-06 23:22:07 +0530

    updated user

--- src/model/User.ts
+++ src/model/User.ts
@@ -19,7 +19,59 @@ const MessageSchema: Schema<Message> = new Schema({
     }
 })
 
-export interface Message extends Document {
-    content: string;
-    createdAt: Date
+export interface User extends Document {
+    username: string;
+    email: string;
+    password: string;
+    verifyCode: string;
+    verifyCodeExpire: Date;
+    isVerified: boolean;
+    isAcceptingMessage: boolean;
+    messages: Message[];
 }
+
+const UserSchema: Schema<User> = new Schema({
+    username: {
+        type: String,
+        required: [true, "Username is required"],
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        match: [/.+@.+\..+/, "Please fill a valid email address"],
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+    },
+    verifyCode: {
+        type: String,
+        required: [true, "Verify code is required"],
+
+    },
+    verifyCodeExpire: {
+        type: Date,
+        required: [true, "Verify code expire is required"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    isAcceptingMessage: {
+        type: Boolean,
+        default: true
+    },
+    messages: {
+        type: [MessageSchema],
+        default: []
+    }
+
+})
+
+const UserModel = (mongoose.model<User>("User", UserSchema));
+
+export default UserModel
